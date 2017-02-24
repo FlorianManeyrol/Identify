aacedf8e22bd3914b442136bed3b174b935daad3
florianmaneyrol <florianmaneyrol@gmail.com>
2017-02-24 13:51:36 +0100

    added blog back end

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.user = current_user
+    if @post.save(post_params)
+      flash[:notice] = "Successfully created post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error creating new post!"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Successfully updated post!"
+      redirect_to post_path(@posts)
+    else
+      flash[:alert] = "Error updating post!"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = "Successfully deleted post!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Error updating post!"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(:id)
+  end
+end
