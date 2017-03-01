80d7db58e5fd633294594382836ef9203f6a9616
florianmaneyrol <florianmaneyrol@gmail.com>
2017-03-01 16:27:04 +0100

    added contact form & designed home page

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,24 @@
+class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Cannot send message. '
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
+end
